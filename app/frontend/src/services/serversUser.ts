57126d08de4df7b8cfdcfd9a5f5f35b064be8206
Fitry28 <egorhohlow1@gmail.com>
2024-10-09 22:19:50 +0400

    init

--- app/frontend/src/services/serversUser.ts
+++ app/frontend/src/services/serversUser.ts
@@ -10,51 +10,17 @@ export function getUserInfo(): string {
   })
 }
 
-// Регестрирует пользователя в базе данных
-export async function registerUser(): Promise<any> {
 
+
+export async function playRoulette(): Promise<any> {
   try {
     const response = await axios({
       method: "post",
-      url: "https://locback.ru.tuna.am/user/create",
-      data: getUserInfo(),
+      url: `https://locback.ru.tuna.am/game/presents?id=${tg.initDataUnsafe.user?.id}&username=${tg.initDataUnsafe.user?.username}`,
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Basic ' + btoa('login:password')
-      }
-    });
-
-    return response.data; // Возвращаем данные от сервера
-  } catch (error: any) {
-    // Обработка ошибки
-    if (axios.isAxiosError(error)) {
-      throw new Error(error.response?.data.message || 'An error occurred during registration');
-    } else {
-      throw new Error('An unexpected error occurred');
-    }
-  }
-}
-
-
-export async function playRoulette(bid: number): Promise<any> {
-  try {
-    const response = await axios({
-      method: "post",
-      url: "https://locback.ru.tuna.am/game/presents",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic ' + btoa('login:password')
       },
-      data: {
-        "deposit": {
-          "deposit": bid
-        },
-        "user": {
-          "id": JSON.parse(getUserInfo()).id
-        }
-      }
     });
 
     return response.data; // Возвращаем данные от сервера
